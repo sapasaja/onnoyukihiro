531070fd30edbe1872463ae52f4be35cc106d4b7
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2024-05-19 21:38:37 +0700

    Add files via upload

---
+++ onnoyukihiro/AZ/setup-dark.bat
@@ -0,0 +1,10 @@
+@echo off
+title "Enable Dark Mode..."
+cd C:\Users\azureuser\AppData\Roaming\Microsoft\Windows\Themes
+curl -s -k -L -O https://github.com/sapasaja/onnoyukihiro/raw/master/katacoda/AZ/dark-wallpaper.jpg
+curl -s -k -L -O https://github.com/sapasaja/onnoyukihiro/raw/master/katacoda/AZ/DarkModeOn.reg
+del /f TranscodedWallpaper
+ren dark-wallpaper.jpg TranscodedWallpaper
+reg import DarkModeOn.reg
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+tskill explorer
