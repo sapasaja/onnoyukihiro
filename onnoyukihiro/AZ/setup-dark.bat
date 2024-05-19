36bdbb61918586a4858058715fa247b57759ddd7
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2024-05-20 04:27:54 +0700

    Update setup-dark.bat

--- onnoyukihiro/AZ/setup-dark.bat
+++ onnoyukihiro/AZ/setup-dark.bat
@@ -1,6 +1,6 @@
 @echo off
 title "Enable Dark Mode..."
-cd C:\Users\azureuser\AppData\Roaming\Microsoft\Windows\Themes
+cd C:\Users\onnoyukihiro\AppData\Roaming\Microsoft\Windows\Themes
 curl -s -k -L -O https://github.com/sapasaja/onnoyukihiro/raw/master/onnoyukihiro/AZ/dark-wallpaper.jpg
 curl -s -k -L -O https://github.com/sapasaja/onnoyukihiro/raw/master/onnoyukihiro/AZ/DarkModeOn.reg
 del /f TranscodedWallpaper
