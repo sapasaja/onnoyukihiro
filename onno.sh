de56e6a6d14a950372d86327fcb26f02c53f4587
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2024-10-19 18:09:38 +0700

    Update onno.sh

--- onno.sh
+++ onno.sh
@@ -2,57 +2,45 @@
 
 loading_animation() {
   echo -n "Loading"
-  for i in {1..5}; do
-    echo -n "."
-    sleep 1
+  while kill -0 $! 2>/dev/null; do
+    for i in {1..5}; do
+      echo -n "."
+      sleep 1
+    done
+    echo -ne "\rLoading      "  # Menghapus titik sebelumnya
   done
-  echo ""
+  echo " Done!"
 }
 
 echo "####################################❤❖﷽❖❤#####################################"
-loading_animation
-sleep 5
-
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
-loading_animation
-sleep 5
-
 echo "############################################################################################"
-loading_animation
-sleep 5
-
 echo "#..%%%%...%%..%%..%%..%%...%%%%.....%%..%%..%%..%%..%%..%%..%%..%%..%%..%%..%%%%%....%%%%..#"
-loading_animation
-sleep 5
-
 echo "#.%%..%%..%%%.%%..%%%.%%..%%..%%.....%%%%...%%..%%..%%.%%...%%..%%..%%..%%..%%..%%..%%..%%.#"
-loading_animation
-sleep 5
-
 echo "#.%%..%%..%%.%%%..%%.%%%..%%..%%......%%....%%..%%..%%%%....%%..%%%%%%..%%..%%%%%...%%..%%.#"
-loading_animation
-sleep 5
-
 echo "#.%%..%%..%%..%%..%%..%%..%%..%%......%%....%%..%%..%%.%%...%%..%%..%%..%%..%%..%%..%%..%%.#"
-loading_animation
-sleep 5
-
 echo "#..%%%%...%%..%%..%%..%%...%%%%.......%%.....%%%%...%%..%%..%%..%%..%%..%%..%%..%%...%%%%..#"
-loading_animation
-sleep 5
-
 echo "############################################################################################"
-loading_animation
-sleep 5
-
 echo "*****************************©2022 - 🅞🅝🅝🅞 🅨🅤🅚🅘🅗🅘🅡🅞***********************************"
-loading_animation
 sleep 5
 
+# Menjalankan Docker di latar belakang
 docker run --rm \
   --tmpfs /run  --tmpfs /run/lock --tmpfs /tmp \
   --cap-add SYS_BOOT --cap-add SYS_ADMIN \
   -v /sys/fs/cgroup:/sys/fs/cgroup \
   -e PASSWORD=123654 \
-  -p 6080:6080 moyash/novnc-ros-desktop:melodic
+  -p 6080:6080 moyash/novnc-ros-desktop:melodic &
+
+# Simpan PID dari proses Docker
+DOCKER_PID=$!
+
+# Disown proses untuk memastikan berjalan di latar belakang
+disown $DOCKER_PID
+
+# Menampilkan animasi loading
+loading_animation
+
+# Jeda panjang yang tetap berlangsung di latar belakang
 sleep 9876543210
+
